8043c3f32c0337d935d85a3b11c289b2b885f3f1
javlonnb <104625519+javlonnb@users.noreply.github.com>
2022-12-26 17:25:28 +0500

    React-JS-Project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Shop } from "./components/Shop";
+import { ToastContainer } from "react-toastify";
+
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+      <Header />
+
+      <Shop />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
